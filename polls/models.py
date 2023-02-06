a6b69f3317e8ec377d7969523d238098246deb70
lutaaya idris <huxaiphaeridris@gmail.com>
2023-02-06 13:08:05 +0300

    Initial commit

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Polls(models.Model):
 
-    name = models.CharField('Name', null=True, blank=True)
+    name = models.CharField('Name', null=True, blank=True, max_length=255)
 
     def __str__(self):
         return self.name
