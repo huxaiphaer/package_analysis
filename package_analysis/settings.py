f48b2467281cd69fce532e3125d54f2bedf626de
lutaaya idris <huxaiphaeridris@gmail.com>
2023-02-06 20:01:30 +0300

    Add django-inlinecss

--- package_analysis/settings.py
+++ package_analysis/settings.py
@@ -34,12 +34,13 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.webdesign',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
     # packages
-    'django_dynamic_fixture',
+    'django_inlinecss',
 
     # apps
     'polls',
